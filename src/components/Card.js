809af5787e6909d4b571ec4d82ff4c84adc1dd68
yamisingh99 <yamisinghsmart@gmail.com>
2024-02-25 06:54:57 +0530

    add more

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,90 +3,39 @@ import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import styled from 'styled-components';
-import Imgcart from '../assets/cardimages/Imgcart.png';
+import Imgcart1 from '../assets/cardimages/Imgcart1.png';
+import Imgcart2 from '../assets/cardimages/Imgcart2.png';
+import Imgcart3 from '../assets/cardimages/Imgcart3.png';
+import Imgcart4 from '../assets/cardimages/Imgcart4.png';
 
 export default function MediaControlCard() {
-  // Get the image width
-  const img = new Image();
-  img.src = Imgcart;
-  const imgWidth = img.width;
+  const imgWidth = 100; // Assuming a constant width for images
+
+  // Array of image paths for each card
+  const images = [Imgcart1,  Imgcart3,Imgcart4, Imgcart2 ];
 
   return (
     <Container>
-      <StyledCard imgWidth={imgWidth}>
-        <CardContentContainer>
-          <CardMedia
-            component="img"
-            sx={{ width: 'auto', height: 'auto' }}
-            image={Imgcart}
-            alt="Live from space album cover"
-          />
-          <CardContentWrapper>
-            <Typography component="div" variant="h8">
-              Live From Space
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary" component="div">
-              Mac Miller
-            </Typography>
-          </CardContentWrapper>
-        </CardContentContainer>
-      </StyledCard>
-      <StyledCard imgWidth={imgWidth}>
-        <CardContentContainer>
-          <CardMedia
-            component="img"
-            sx={{ width: 'auto', height: 'auto' }}
-            image={Imgcart}
-            alt="Live from space album cover"
-          />
-          <CardContentWrapper>
-            <Typography component="div" variant="h8">
-              Live From Space
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary" component="div">
-              Mac Miller
-            </Typography>
-          </CardContentWrapper>
-        </CardContentContainer>
-      </StyledCard>
-      <StyledCard imgWidth={imgWidth}>
-        <CardContentContainer>
-          <CardMedia
-            component="img"
-            sx={{ width: 'auto', height: 'auto' }}
-            image={Imgcart}
-            alt="Live from space album cover"
-          />
-          <CardContentWrapper>
-            <Typography component="div" variant="h8">
-              Live From Space
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary" component="div">
-              Mac Miller
-            </Typography>
-          </CardContentWrapper>
-        </CardContentContainer>
-      </StyledCard>
-      <StyledCard imgWidth={imgWidth}>
-        <CardContentContainer>
-          <CardMedia
-            component="img"
-            sx={{ width: 'auto', height: 'auto' }}
-            image={Imgcart}
-            alt="Live from space album cover"
-          />
-          <CardContentWrapper>
-            <Typography component="div" variant="h8">
-              Live From Space
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary" component="div">
-              Mac Miller
-            </Typography>
-          </CardContentWrapper>
-        </CardContentContainer>
-      </StyledCard>
-     
-      
+      {images.map((image, index) => (
+        <StyledCard key={index} imgWidth={imgWidth}>
+          <CardContentContainer>
+            <CardMedia
+              component="img"
+              sx={{ width: 'auto', height: 'auto' }}
+              image={image} // Use the corresponding image for each card
+              alt={`Card ${index + 1}`}
+            />
+            <CardContentWrapper>
+              <Typography component="div" variant="h8">
+                Title {index + 1}
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                Subtitle {index + 1}
+              </Typography>
+            </CardContentWrapper>
+          </CardContentContainer>
+        </StyledCard>
+      ))}
     </Container>
   );
 }
@@ -94,8 +43,8 @@ export default function MediaControlCard() {
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  background-color: #87CEFA; 
-  padding: 1rem;   
+  background-color: #87CEFA;
+  padding: 1rem;
 `;
 
 const StyledCard = styled(Card)`
